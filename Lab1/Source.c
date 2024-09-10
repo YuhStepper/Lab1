9767f075cb3a7bc49cde050f0caae724e1f0a4fe
Admin <Admin@JOVERUS>
2024-09-10 23:02:32 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+
+{
+	setlocale(LC_CTYPE, "RUS");
+
+	puts("  *               *   ");
+	puts(" * *             * *  ");
+	puts("* ^ *           * ^ * ");
+	puts("*    * * * * * *    *");
+	puts("*                   *");
+	puts("*  ( * )     ( * )  *");
+	puts("*                   *");
+	puts("*         ^         *");
+	puts("*      >__!__<      *");
+	puts("*                   *");
+	puts(" *                 * ");
+	puts("  * * * * * * * * *   ");
+}
